4c5d4506d57dc49163be726a13a6e2e21340c4f0
vendetta60 <115636143+vendetta60@users.noreply.github.com>
2025-12-01 08:59:13 +0400

    Refactor argument handling in 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
-    argc = len(argv) - 1
-    if argc == 0:
+    import sys
+
+    argv = sys.argv[1:]
+    count = len(argv)
+
+    if count == 0:
         print("0 arguments.")
-    elif argc == 1:
-        print("1 argument:\n1: {}".format(argv[1]))
     else:
-        print("{} arguments:".format(argc))
-        for i in range(1, argc + 1):
-            print("{}: {}".format(i, argv[i]))
+        print("{} argument{}:".format(count, "" if count == 1 else "s"))
+        for i in range(count):
+            print("{}: {}".format(i + 1, argv[i]))
