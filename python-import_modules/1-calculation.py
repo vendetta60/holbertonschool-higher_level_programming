c0244ffb65f6f95a192762520f7b6c0e0477002a
vendetta60 <115636143+vendetta60@users.noreply.github.com>
2025-12-01 08:57:09 +0400

    Refactor to handle command line arguments
    Modified the script to accept command line arguments and display their count and values.

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    a = 10
-    b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    import sys
+
+    argv = sys.argv[1:]
+    count = len(argv)
+
+    if count == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(count, "" if count == 1 else "s"))
+        for i in range(count):
+            print("{}: {}".format(i + 1, argv[i]))
