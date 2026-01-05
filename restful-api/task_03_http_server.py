1eb4f771ebf507a6e62b966476f47e229037f7dd
vendetta60 <115636143+vendetta60@users.noreply.github.com>
2026-01-05 10:33:11 +0400

    Update shebang and byte string usage in HTTP server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Module to implement http.server module"""
+
 import http.server
 import json
 
@@ -10,14 +11,12 @@ class HTTPHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         """Method to handle GET requests"""
 
-        # Base case
         if self.path == '/':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write('Hello, this is a simple API!'.encode())
+            self.wfile.write(b"Hello, this is a simple API!")
 
-        # If /data is accessed
         elif self.path == '/data':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
@@ -29,30 +28,30 @@ class HTTPHandler(http.server.BaseHTTPRequestHandler):
             }
             self.wfile.write(json.dumps(data).encode('utf-8'))
 
-        # /status endpoint
         elif self.path == '/status':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write("OK".encode('utf-8'))
+            self.wfile.write(b"OK")
 
         elif self.path == '/info':
-            self._set_headers(content_type='application/json')
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
             data = {
                 "version": "1.0",
                 "description": "A simple API built with http.server"
             }
             self.wfile.write(json.dumps(data).encode('utf-8'))
 
-        # Error 404 :)
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write('404 Not Found'.encode())
+            self.wfile.write(b"Endpoint not found")
+
 
 if __name__ == '__main__':
-    """Server initialization"""
     server_address = ('', 8000)
     httpserver = http.server.HTTPServer(server_address, HTTPHandler)
     httpserver.serve_forever()
